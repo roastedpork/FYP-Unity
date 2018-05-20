2ec562cc3b3606435dd5bcdf0d127f005c5741a3
Chan Bingjie <jienet888@gmail.com>
2018-05-20 12:39:29 +0100

    fixed minimap and arrows relative to headset

---
+++ Scripts/DisplayElements/UserHeadset.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserHeadset : MonoBehaviour {
+
+    private GameObject cam;
+    
+	// Use this for initialization
+	void Start () {
+        cam = GameObject.FindGameObjectWithTag("MainCamera");
+
+        if (cam != null)
+        {
+            Debug.Log("[UserHeadset] Camera found");
+            transform.parent = cam.transform;
+            transform.localRotation = Quaternion.Euler(0, -90, 0);
+        }
+        else Debug.Log("[UserHeadset] Camera could not be found");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+    }
+}
